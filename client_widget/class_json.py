4fce6d3006d1a023354fcf3bdfaca63cfe74865f
Rocco <grog512new@mail.ru>
2024-02-29 01:05:21 +0300

    Добавлена функция format_from_account москирует номер карты в формате XXXX XX** **** XXXX и счета в формате **XXXX

--- client_widget/class_json.py
+++ client_widget/class_json.py
@@ -16,3 +16,21 @@ class Operation:
         self.description = data.get("description")
         self.from_account = data.get("from")
         self.to_account = data.get("to")
+
+
+    def format_from_account(self):
+        if self.from_account:
+            digits_from_account = ''.join(c for c in self.from_account if c.isdigit())
+            letters_from_account = ''.join(c for c in self.from_account if c.isalpha())
+            if len(digits_from_account) == 16:
+                formatted_account = " ".join([digits_from_account[i:i + 4] for i in range(0, 16, 4)])
+                formatted_account = f"{formatted_account[:-12]}** **** {formatted_account[-4:]}"
+                return letters_from_account + ' ' + formatted_account
+            elif len(digits_from_account) == 20:
+                formatted_account = digits_from_account[:-16]
+                formatted_account = f"**{formatted_account}"
+                return formatted_account
+            else:
+                return self.from_account
+        else:
+            return f"Нет данных. Открытие вклада"
